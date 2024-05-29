618f96ab50d4c279cd369e124ed7cf6d774a7e60
Romualdo "Romuboy" Santos <romualdoosfilho94@gmail.com>
2024-05-29 16:22:01 -0300

    ladders

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,40 @@
 extends CharacterBody2D
 class_name Player
 
+enum { MOVE, CLIMB }
+
 @export var moveDate : Resource
+
+var state = MOVE
+
+@onready var ladder_check = $LadderCheck
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _ready():
 	$AnimatedSprite2D.frames = load("res://assets/frames/playerGreenSkin.tres")
 
 func _physics_process(delta):
+	# Get the input direction and handle the movement/deceleration.
+	var direction = Vector2.ZERO
+	direction.x = Input.get_axis("move_left", "move_right")
+	direction.y = Input.get_axis("move_up", "move_down")
 	
+	match state:
+		MOVE: move_state(direction.x, delta)
+		CLIMB: climb_state(direction)
+	
+	
+	
+
+func move_state(direction, delta):
+	if is_on_ladder() and Input.is_action_just_pressed("move_up"):
+		state = CLIMB
 	# Add the gravity.
 	if not is_on_floor():
-		apply_gravity(gravity * delta)		
+		apply_gravity(gravity * delta)
 		$AnimatedSprite2D.play("jump")
-	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
 	
 	if direction:
 		apply_aceleration(direction)
@@ -32,10 +47,10 @@ func _physics_process(delta):
 			$AnimatedSprite2D.play("idle")
 		
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = moveDate.JUMP_VELOCITY
 	else:
-		if Input.is_action_just_released("ui_accept") and velocity.y < moveDate.jump_release_force:
+		if Input.is_action_just_released("jump") and velocity.y < moveDate.jump_release_force:
 			velocity.y = moveDate.jump_release_force
 		if velocity.y > 0:
 			velocity.y += moveDate.additional_fall_gravity
@@ -46,6 +61,29 @@ func _physics_process(delta):
 	if just_landed:
 		$AnimatedSprite2D.animation = "run"
 		$AnimatedSprite2D.frame = 1
+
+func climb_state(direction):
+	if not is_on_ladder():
+		state = MOVE
+	
+	if direction.length() != 0:
+		$AnimatedSprite2D.play("run")
+	else:
+		$AnimatedSprite2D.play("idle")
+	
+	velocity = direction * 50
+	move_and_slide()
+
+func is_on_ladder():
+	if not ladder_check.is_colliding(): 
+		return false
+	
+	var collider = ladder_check.get_collider()
+	
+	if not collider is Ladder: 
+		return false
+		
+	return true
 
 func apply_gravity(gravity):
 	velocity.y += gravity
